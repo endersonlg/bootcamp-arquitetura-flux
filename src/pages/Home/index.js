99772db6b3628f2a34c377af44fe7963135139e8
Enderson Lúcio Garcia <55244861+endersonlg@users.noreply.github.com>
2020-03-21 03:35:28 -0300

    20 - Configurando Redux Saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,10 +25,10 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = (product) => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   }
   render() {
     const { products } = this.state;
@@ -44,7 +44,7 @@ class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button" onClick={() => this.handleAddProduct(product)}>
+            <button type="button" onClick={() => this.handleAddProduct(product.id)}>
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> {' '}
                 {amount[product.id] || 0}
